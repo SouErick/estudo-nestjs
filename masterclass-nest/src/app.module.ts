073c96c92eb4bac5093c7f78f72b45b6cbf3e65e
SouErick <ericksousasaraiva@gmail.com>
2026-09-07 15:39:41 -0300

    estudo 5: conceitos de middleware implementados para logging da aplicacao

--- masterclass-nest/src/app.module.ts
+++ masterclass-nest/src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { FilesModule } from './files/files.module';
+import { LoggerMiddleware } from './app.middleware';
 
 @Module({
   imports: [AuthModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer
+    .apply(LoggerMiddleware)
+    .forRoutes('*');
+  }
+} 
